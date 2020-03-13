c906a0ff413282873f0dc0e998558b74a8be81a6
Chiara Argese <argese.chiara@gmail.com>
2020-03-13 11:03:44 +0000

    add comments never hurts

--- scripts/csv2xml_pos.py
+++ scripts/csv2xml_pos.py
@@ -14,7 +14,7 @@ from lxml.etree import Element, SubElement, XMLParser
 read_file = sys.argv[1]
 
 pos_dict = {}
-entry_array = []
+# Define an Entry class containing lemma, pos, gender, lemma synonym, translation, and translation pos
 class Entry:
     def __init__(self, lemma, pos, gen, lem_syn, trans, trans_pos):
         self.lemma = lemma
@@ -24,10 +24,12 @@ class Entry:
         self.trans = trans
         self.trans_pos = trans_pos
 
+# Read the csv file
 with open(read_file) as f:
     lines = f.readlines()
 f.close()
 
+# Create a dictionary of entries collected by pos
 for i in range(1, len(lines)):
     line = lines[i].split('\t')
     pos = line[4]
@@ -37,12 +39,14 @@ for i in range(1, len(lines)):
         el = pos_dict[pos]
         pos_dict[pos].append(Entry(line[0], line[4], line[1], line[2], line[9], line[11]))
 
+# Create an xml file per pos by reading the dictionary (dict_pos)
 for key, value in pos_dict.items():
     out_file = key.replace(" ", "_").replace("/", "#").upper() + "_spasme.xml"
     write_file = open(out_file,"a+")
     out_tree = Element("r")
     added = []
     for val in value:
+        # create first entry with lemma = val.lemma
         if not val.lemma in added:
             e_elem = SubElement(out_tree, "e")
             lg_elem = SubElement(e_elem, "lg")
@@ -60,6 +64,7 @@ for key, value in pos_dict.items():
             t_elem.text = val.trans
             added.append(val.lemma)
         else:
+            # select the already existing entry with lemma = val.lemma and add mg with different translation
             for l in out_tree.getiterator("l"):
                 if l.text == val.lemma:
                     mg_elem = SubElement((l.getparent()).getparent(), "mg")
