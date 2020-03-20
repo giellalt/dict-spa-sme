0e92cd1627918bb05f45139e55ae8a5de7431f18
Chiara Argese <argese.chiara@gmail.com>
2020-03-20 15:48:11 +0000

    do not create empty tag <syng/> and <xg/>, otherwise get parsing error in NDS

--- scripts/csv2xml_pos.py
+++ scripts/csv2xml_pos.py
@@ -8,6 +8,7 @@ For each pos, an xml file named <POS>_spasme.xml is generated in this folder.
 
 import sys
 import csv
+import lxml.etree
 from lxml.etree import ElementTree as ET
 from lxml.etree import Element, SubElement, XMLParser
 
@@ -52,13 +53,16 @@ class Entry:
         self.saami_ex_7 = saami_ex_7
 
 # Check if value exists, if yes create element (optional t_element to add saami_ex together with spa_ex)
-def check_and_insert(value, parent, tag_name, t_element=None):
+#def check_and_insert(value, parent, tag_name, t_element=None):
+def check_and_insert(value, parent, tag_name, ppar=None, ppar_tag_name=None, t_element=None):
     if value:
+        if ppar and ppar_tag_name:
+            parent = SubElement(ppar, ppar_tag_name)
         element = SubElement(parent, tag_name)
         element.text = value
         if t_element:
             if t_element[0]:
-                element = SubElement(t_element[1], t_element[2])
+                element = SubElement(parent, t_element[1])
                 element.text = t_element[0]
     return
 
@@ -79,7 +83,7 @@ for i in range(1, len(lines)):
 
 # Create an xml file per pos by reading the dictionary (dict_pos)
 for key, value in pos_dict.items():
-    # sort according translation number 
+    # sort according translation number
     value.sort(key=lambda x: x.trans_num)
     out_file = "../src/" + key.replace(" ", "_").replace("/", "#").upper() + "_spasme.xml"
     write_file = open(out_file,"a+")
@@ -105,21 +109,21 @@ for key, value in pos_dict.items():
             t_elem.set("re", val.restr)
             t_elem.set("sci", val.sci_name)
             t_elem.text = val.saami
-            xg_elem = SubElement(tg_elem, "xg")
-            check_and_insert(val.spa_ex_1, xg_elem, "x", [val.saami_ex_1, xg_elem, "xt"])
-            check_and_insert(val.spa_ex_2, xg_elem, "x", [val.saami_ex_2, xg_elem, "xt"])
-            check_and_insert(val.spa_ex_3, xg_elem, "x", [val.saami_ex_3, xg_elem, "xt"])
-            check_and_insert(val.spa_ex_4, xg_elem, "x", [val.saami_ex_4, xg_elem, "xt"])
-            check_and_insert(val.spa_ex_5, xg_elem, "x", [val.saami_ex_5, xg_elem, "xt"])
-            check_and_insert(val.spa_ex_6, xg_elem, "x", [val.saami_ex_6, xg_elem, "xt"])
-            check_and_insert(val.spa_ex_7, xg_elem, "x", [val.saami_ex_7, xg_elem, "xt"])
-            syng_elem = SubElement(mg_elem, "syng")
-            check_and_insert(val.trans_syn_1, syng_elem, "syn")
-            check_and_insert(val.trans_syn_2, syng_elem, "syn")
-            check_and_insert(val.trans_syn_3, syng_elem, "syn")
-            check_and_insert(val.trans_syn_4, syng_elem, "syn")
-            check_and_insert(val.trans_syn_5, syng_elem, "syn")
-            check_and_insert(val.trans_syn_6, syng_elem, "syn")
+            #xg_elem = SubElement(tg_elem, "xg")
+            check_and_insert(val.spa_ex_1, "", "x", tg_elem, "xg", [val.saami_ex_1, "xt"])
+            check_and_insert(val.spa_ex_2, "", "x", tg_elem, "xg", [val.saami_ex_2, "xt"])
+            check_and_insert(val.spa_ex_3, "", "x", tg_elem, "xg", [val.saami_ex_3, "xt"])
+            check_and_insert(val.spa_ex_4, "", "x", tg_elem, "xg", [val.saami_ex_4, "xt"])
+            check_and_insert(val.spa_ex_5, "", "x", tg_elem, "xg", [val.saami_ex_5, "xt"])
+            check_and_insert(val.spa_ex_6, "", "x", tg_elem, "xg", [val.saami_ex_6, "xt"])
+            check_and_insert(val.spa_ex_7, "", "x", tg_elem, "xg", [val.saami_ex_7, "xt"])
+            #syng_elem = SubElement(mg_elem, "syng")
+            check_and_insert(val.trans_syn_1, "", "syn", mg_elem, "syng")
+            check_and_insert(val.trans_syn_2, "", "syn", mg_elem, "syng")
+            check_and_insert(val.trans_syn_3, "", "syn", mg_elem, "syng")
+            check_and_insert(val.trans_syn_4, "", "syn", mg_elem, "syng")
+            check_and_insert(val.trans_syn_5, "", "syn", mg_elem, "syng")
+            check_and_insert(val.trans_syn_6, "", "syn", mg_elem, "syng")
             added.append(val.word)
         else:
             # select the already existing entry with word = val.word and add mg with different translation
@@ -134,20 +138,21 @@ for key, value in pos_dict.items():
                     t_elem.set("re", val.restr)
                     t_elem.set("sci", val.sci_name)
                     t_elem.text = val.saami
-                    xg_elem = SubElement(tg_elem, "xg")
-                    check_and_insert(val.spa_ex_1, xg_elem, "x", [val.saami_ex_1, xg_elem, "xt"])
-                    check_and_insert(val.spa_ex_2, xg_elem, "x", [val.saami_ex_2, xg_elem, "xt"])
-                    check_and_insert(val.spa_ex_3, xg_elem, "x", [val.saami_ex_3, xg_elem, "xt"])
-                    check_and_insert(val.spa_ex_4, xg_elem, "x", [val.saami_ex_4, xg_elem, "xt"])
-                    check_and_insert(val.spa_ex_5, xg_elem, "x", [val.saami_ex_5, xg_elem, "xt"])
-                    check_and_insert(val.spa_ex_6, xg_elem, "x", [val.saami_ex_6, xg_elem, "xt"])
-                    check_and_insert(val.spa_ex_7, xg_elem, "x", [val.saami_ex_7, xg_elem, "xt"])
-                    syng_elem = SubElement(mg_elem, "syng")
-                    check_and_insert(val.trans_syn_1, syng_elem, "syn")
-                    check_and_insert(val.trans_syn_2, syng_elem, "syn")
-                    check_and_insert(val.trans_syn_3, syng_elem, "syn")
-                    check_and_insert(val.trans_syn_4, syng_elem, "syn")
-                    check_and_insert(val.trans_syn_5, syng_elem, "syn")
-                    check_and_insert(val.trans_syn_6, syng_elem, "syn")
+                    #xg_elem = SubElement(tg_elem, "xg")
+                    check_and_insert(val.spa_ex_1, "", "x", tg_elem, "xg", [val.saami_ex_1, "xt"])
+                    check_and_insert(val.spa_ex_2, "", "x", tg_elem, "xg", [val.saami_ex_2, "xt"])
+                    check_and_insert(val.spa_ex_3, "", "x", tg_elem, "xg", [val.saami_ex_3, "xt"])
+                    check_and_insert(val.spa_ex_4, "", "x", tg_elem, "xg", [val.saami_ex_4, "xt"])
+                    check_and_insert(val.spa_ex_5, "", "x", tg_elem, "xg", [val.saami_ex_5, "xt"])
+                    check_and_insert(val.spa_ex_6, "", "x", tg_elem, "xg", [val.saami_ex_6, "xt"])
+                    check_and_insert(val.spa_ex_7, "", "x", tg_elem, "xg", [val.saami_ex_7, "xt"])
+                    #syng_elem = SubElement(mg_elem, "syng")
+                    check_and_insert(val.trans_syn_1, "", "syn", mg_elem, "syng")
+                    check_and_insert(val.trans_syn_2, "", "syn", mg_elem, "syng")
+                    check_and_insert(val.trans_syn_3, "", "syn", mg_elem, "syng")
+                    check_and_insert(val.trans_syn_4, "", "syn", mg_elem, "syng")
+                    check_and_insert(val.trans_syn_5, "", "syn", mg_elem, "syng")
+                    check_and_insert(val.trans_syn_6, "", "syn", mg_elem, "syng")
+
     ET(out_tree).write(out_file, encoding="UTF-8", pretty_print=True)
     write_file.close()
