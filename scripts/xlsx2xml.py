9962ddb9f24b82e7b35b0d374197514b10913e13
trondtynnol <tty001@uit.no>
2025-11-06 11:33:51 +0100

    .venv > venv to harmonize with what NDS expects

--- scripts/xlsx2xml.py
+++ scripts/xlsx2xml.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 MISSING_DEP_HELP = """
 cannot run due to missing dependencies. hint, run:
-python -m venv .venv && . .venv/bin/activate && pip install -r xlsx2xml-requirements.txt
+python -m venv venv && . venv/bin/activate && pip install -r xlsx2xml-requirements.txt
 ...and then try again. (remember to run `deactivate` in the shell when you're done)
 """
 
